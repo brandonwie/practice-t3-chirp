752e8d7c01a9a3b5152351e902fe3cb2a533ebab
Brandon Wie <brandonwie.cs@gmail.com>
2023-04-20 15:53:38 +0900

    Setting

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
-import { postsRouter } from "./routers/postRouter";
+import { postsRouter } from "./routers/posts";
 
 /**
  * This is the primary router for your server.
