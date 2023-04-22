81181b53a8bcfe695d87d97f6bb65747629dfcc5
Brandon Wie <brandonwie.cs@gmail.com>
2023-04-22 13:45:12 +0900

    Profile page with ssg helper

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,17 +1,72 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+import { PageLayout } from "~/components/layout";
 import Head from "next/head";
+import Image from "next/image";
+import { api } from "../utils/api";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
-      <main className={"flex justify-center"}>
-        <div>Profile View</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? "unknown"}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl">{`@${data.username ?? ""}`}</div>
+        <div className="border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
+};
+
+// export const getServerSideProps = async (context) => {
+//   // run every request on this page - block every request & time consuming
+//   // not gonna have cache page
+// } => use SSG
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+  if (typeof slug !== "string") throw new Error("No slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default ProfilePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import { LoadingPage, LoadingSpinner } from "../components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import Link from "next/link";
+import { PageLayout } from "~/components/layout";
 
 dayjs.extend(relativeTime);
 
@@ -126,19 +127,18 @@ const Home: NextPage = () => {
   if (!userLoaded) return <div />;
 
   return (
-    <main className={"flex justify-center"}>
-      <div className={"w-full border-x border-slate-400 md:max-w-2xl"}>
-        <div className={"flex border-b border-slate-400 p-4"}>
-          {!isSignedIn && (
-            <div className="flex justify-center">
-              <SignInButton />
-            </div>
-          )}
-          {isSignedIn && <CreatePostWizard />}
-        </div>
-        <Feed />
+    <PageLayout>
+      <div className={"flex border-b border-slate-400 p-4"}>
+        {!isSignedIn && (
+          <div className="flex justify-center">
+            <SignInButton />
+          </div>
+        )}
+        {isSignedIn && <CreatePostWizard />}
       </div>
-    </main>
+
+      <Feed />
+    </PageLayout>
   );
 };
 
